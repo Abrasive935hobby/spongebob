ceb98d2ac6b386e47f6b4226c66fdc7c5b3bbb8b
Abrasive935hobby <malcolmedwards2007@gmail.com>
2025-10-09 15:37:39 -0300

    added activity thing

---
+++ student_number_prog1700-lists-01.py
@@ -0,0 +1,6 @@
+movies = ["Cars", "The Matrix", "Spiderman 2", "The Dark Knight", "Madagascar"]
+print("All movies:", movies)
+print("First movie:", movies[0])
+print("Last movie:", movies[-1])
+movies[2] = "Sonic 3"
+print("Updated movies:", movies)
